01e365fc315ab4d7b8bcefad7490e75b69404be0
kaito <kt1998sphere@gmail.com>
2025-03-23 20:37:36 +0900

    Initial commit

---
+++ my-garden/my_gardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_gardenApp.swift
+//  my-garden
+//
+//  Created by 竹村 海人 on 2025/03/23.
+//
+
+import SwiftUI
+
+@main
+struct my_gardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ my-gardenTests/my_gardenTests.swift
@@ -0,0 +1,16 @@
+//
+//  my_gardenTests.swift
+//  my-gardenTests
+//
+//  Created by 竹村 海人 on 2025/03/23.
+//
+
+import Testing
+
+struct my_gardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
